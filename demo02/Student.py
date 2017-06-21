95a138bb74e18a929ee35e820e6fb20557b86b42
蘇子耀 <simplesuhao@163.com>
2017-06-21 23:34:44 +0800

    add

--- demo02/Student.py
+++ demo02/Student.py
@@ -2,4 +2,13 @@
 # -*- coding: utf-8 -*-
 ' a test module '
 class Student(object):
-    pass
+    ''' init方法的第一个参数永远是self'''
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+bart = Student('Bart Simpson', 59)
+print bart
+print Student
+
+print bart.score
