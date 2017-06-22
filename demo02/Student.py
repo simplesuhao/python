729c367c1ad6584f40c79b62d9661aedb999d624
su_xiao <simplesuhao@163.com>
2017-06-22 14:36:30 +0800

    access restriction & get set mothed

--- demo02/Student.py
+++ demo02/Student.py
@@ -2,13 +2,33 @@
 # -*- coding: utf-8 -*-
 ' a test module '
 class Student(object):
-    ''' init方法的第一个参数永远是self'''
+    '''init方法的第一个参数永远是self'''
     def __init__(self, name, score):
-        self.name = name
-        self.score = score
-
-bart = Student('Bart Simpson', 59)
-print bart
-print Student
-
-print bart.score
+        '''add access restriction'''
+        self.__name = name
+        self.__score = score
+    '''对数据进行封装'''
+    def print_score(self):
+        print '%s: %s' % (self.__name, self.__score)
+    '''增加新的方法'''
+    def get_grade(self):
+        if self.__score >= 90:
+            return 'A'
+        elif self.__score >=60:
+            return 'B'
+        else:
+            return 'C'
+    '''增加get&set方法'''
+    def get_name(self):
+        return self.__name
+    def get_score(self):
+        return self.__score
+    def set_score(self, score):
+        if 0 <= score <= 100:
+            self.__score = score
+        else:
+            raise ValueError('Bad score')
+bart = Student('vva', 89)
+print bart.get_score()
+bart.print_score()
+print bart.get_grade()
